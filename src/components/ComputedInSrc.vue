59b6a454efbdd8c579cfc2af39a78f752c174861
Oskar <oskar.baumann@tum.de>
2021-10-08 14:41:41 +0200

    Initial Commit

---
+++ src/components/ComputedInSrc.vue
@@ -0,0 +1,14 @@
+<template>
+    <img alt="Vue logo" :src="require('../assets/logo.png')" />
+
+    <!-- This works with serve and with build -->
+    <img alt="Vue logo" :src="require('../assets/' + imageSrc)" />
+
+    <!-- This works with serve not with build -> ReferenceError: _unref is not defined -->
+    <img alt="Vue logo" :src="require(`../assets/${imageSrc}`)" />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+const imageSrc = computed(() => { return 'logo.png' })
+</script>
